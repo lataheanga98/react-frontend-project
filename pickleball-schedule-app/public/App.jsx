073c6bb37ee68aa05ae7d5d975aa18f8cf569987
Lata <151824931+lataheanga98@users.noreply.github.com>
2025-05-28 03:01:59 +0000

    Second events

--- pickleball-schedule-app/public/App.jsx
+++ pickleball-schedule-app/public/App.jsx
@@ -10,6 +10,10 @@ const events = [
         location: "Vava'u Courts", 
     },
     {
-        
-    }
+        title: "Vava'u Club Tournaments",
+        date: "2025-06-05",
+        time: "2:00 PM - 6:00 PM",
+        location: "Saineha High School Gym",
+    },
+
 ]
