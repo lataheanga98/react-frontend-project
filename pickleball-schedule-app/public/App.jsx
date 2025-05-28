eeba6dcb82c02bc97f0065c5da4012615cb6eda2
Lata <151824931+lataheanga98@users.noreply.github.com>
2025-05-28 03:00:05 +0000

    Added in the first events.

--- pickleball-schedule-app/public/App.jsx
+++ pickleball-schedule-app/public/App.jsx
@@ -1,3 +1,15 @@
 import React from 'react';
 import './App.css';
 import EventCard from '/components/EventCard';
+
+const events = [
+    {
+        title: "Morning Open Play",
+        date: "2025-06-01",
+        time: "9:00 AM - 11:00 AM",
+        location: "Vava'u Courts", 
+    },
+    {
+        
+    }
+]
